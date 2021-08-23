a8e83d45d764f84a0de20cbef6418d339df30948
Leul Tefera <leulmulugeta1@gmail.com>
2021-08-23 09:17:15 -0500

    formated the code and minor corrections

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 document.querySelector(".get-jokes").addEventListener("click", getJokes);
 
 function getJokes (e) {
+  // Get the amount of jokes requested 
   let number = document.querySelector("#num").value;
 
   const xhr = new XMLHttpRequest();
@@ -12,24 +13,20 @@ function getJokes (e) {
     if(this.status === 200) {
 
       const response = JSON.parse(this.responseText);
-      // Loop 
+      // Loop through the list of arrays inside the returned object and concatinate the jokes inside output
 
       output = '';
 
       if(response.type === 'success') {
         response.value.forEach(function(joke) {
-          output += `<li>${joke.joke}</li>`;
+          output += `<li>"${joke.joke}"</li>`;
         });
       }else {
-        output += '<li> Something Went wrong</li>'
+        output += '<h2>There is an issue processing the jokes, please try again later.</h2>'
       }
       document.querySelector('.jokes').innerHTML = output;
     }
   }
-
-
   xhr.send();
-
-
   e.preventDefault();
 }
